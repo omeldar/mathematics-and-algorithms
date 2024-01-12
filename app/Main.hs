5565b01b571f5b430579ba091a9dc216b64db8f3
omeldar <eldar.work.edu@gmail.com>
2024-01-13 00:46:37 +0100

    Add more examples to app Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,15 @@
 module Main where
 
 import ModularArithmetic
+import Data.Maybe (fromJust)
 
 main :: IO ()
 main = do
   -- Square and Multiply
   print "Square and Multiply:"
   print $ ModularArithmetic.squareandmultiply 252533 18168396 33554393
+  print $ ModularArithmetic.squareandmultiply 855 2753 61
+  print $ ModularArithmetic.squareandmultiply 855 2753 53
 
   -- Extended Euclidean Algorithm
   print "Extended Euclidean Algorithm:"
@@ -18,11 +21,13 @@ main = do
 
   -- Modular Inverse
   print "Modular Inverse:"
-  print $ ModularArithmetic.modInverse 2 19
-  print $ ModularArithmetic.modInverse (-3) 8
-  print $ ModularArithmetic.modInverse 8 11
+  print $ fromJust $ ModularArithmetic.modInverse 2 19
+  print $ fromJust $ ModularArithmetic.modInverse (-3) 8
+  print $ fromJust $ ModularArithmetic.modInverse 17 3120
+  print $ fromJust $ ModularArithmetic.modInverse 1721 262548
   
   -- Chinese Remainder Theorem
   print "Chinese Remainder Theorem:"
   print $ ModularArithmetic.chineseremainder [(1, 10), (2, 3), (6, 7)]
   print $ ModularArithmetic.chineseremainder [(1, 2), (2, 3), (3, 5), (4, 11)]
+  print $ ModularArithmetic.chineseremainder [(1, 61), (17, 53)]
